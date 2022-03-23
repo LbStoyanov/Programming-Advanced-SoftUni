997c11a2601ca5b6014fc7f9baa684a2c38ca2e9
Liubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-23 17:32:14 +0200

    New task added, old one completed

--- 06. Songs Queue/Program.cs
+++ 06. Songs Queue/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06._Songs_Queue
 {
@@ -6,7 +8,65 @@ namespace _06._Songs_Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] inicialSongs = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> queue = new Queue<string>();
+
+            for (int i = 0; i < inicialSongs.Length; i++)
+            {
+                queue.Enqueue(inicialSongs[i]);
+            }
+
+            string commands = Console.ReadLine();
+
+            while (queue.Count > 0)
+            {
+                string[] actions = commands.Split(" ").ToArray();
+                string mainAction = actions[0];
+
+                if (mainAction == "Play" && queue.Count > 0)
+                {
+                    queue.Dequeue();
+                }
+                else if (mainAction == "Add")
+                {
+                    int startIndex = 4;
+                    int endIndex = commands.Length - 1;
+
+                    string newSong = commands.Substring(startIndex,endIndex - startIndex + 1);
+                    if (!queue.Contains(newSong))
+                    {
+                        queue.Enqueue(newSong);
+                    }
+                    else
+                    {
+                        
+                        Console.WriteLine($"{newSong} is already contained!");
+                    }
+                }
+                else
+                {
+                    var currentSongsList = queue.ToList();
+                    Console.WriteLine(String.Join(", ",currentSongsList));
+                    
+                }
+
+                commands = Console.ReadLine();
+            }
+
+            var listOfSongs = queue.ToList();
+
+            if (listOfSongs.Count != 0)
+            {
+                Console.WriteLine(String.Join(", ", listOfSongs));
+                Console.WriteLine("No more songs!");
+            }
+            else
+            {
+                Console.WriteLine("No more songs!");
+            }
+
+            
         }
     }
 }
