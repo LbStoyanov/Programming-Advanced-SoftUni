c8e7c1d1075d6ae557d1ddd40b53fc832a120a21
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-24 17:41:23 +0200

    New task added, Previuos is resolved

--- 10. Crossroads/Program.cs
+++ 10. Crossroads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10._Crossroads
 {
@@ -6,7 +8,75 @@ namespace _10._Crossroads
     {
         static void Main(string[] args)
         {
-            
+            int durationOfTheGreenlight = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            Queue<string> carsQueue = new Queue<string>();
+            int carsPassedTheCrossRoad = 0;
+
+            while (command != "END")
+            {
+                if (command == "green")
+                {
+
+                    int currentDurationOfGreen = durationOfTheGreenlight;
+
+                    while (currentDurationOfGreen > 0)
+                    {
+                        
+                        if (carsQueue.Count == 0)
+                        {
+                            break;
+                        }
+
+                        var currentCar = carsQueue.Peek();
+
+                        if (currentDurationOfGreen - currentCar.Length > 0)
+                        {
+                            carsQueue.Dequeue();
+                            currentDurationOfGreen -= currentCar.Length;
+                            carsPassedTheCrossRoad++;
+                        }
+                        else
+                        {
+
+                            int carLenght = currentCar.Length - currentDurationOfGreen;
+
+                            if (freeWindow < carLenght)
+                            {
+                                int count = carLenght - freeWindow;
+
+                                int index = currentCar.Length - count;
+
+                                char crash = currentCar.ElementAt(index);
+
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{currentCar} was hit at {crash}.");
+                                return;
+                            }
+                            else
+                            {
+                                carsQueue.Dequeue();
+                                carsPassedTheCrossRoad++;
+                                break;
+                                
+                            }
+
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(command);
+                }
+                
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carsPassedTheCrossRoad} total cars passed the crossroads.");
         }
     }
 }
