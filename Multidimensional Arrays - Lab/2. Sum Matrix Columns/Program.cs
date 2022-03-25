25f62db379b9dd2d5accb6f505ff70b9bb6bcb10
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-25 17:49:41 +0200

    02.Provblem Resolved, new problem added

--- Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
+++ Multidimensional Arrays - Lab/2. Sum Matrix Columns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _2._Sum_Matrix_Columns
 {
@@ -6,7 +7,37 @@ namespace _2._Sum_Matrix_Columns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+            int sum = 0;
+
+            int[,] matrix = new int[rows, cols];
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    //Console.WriteLine($"Reading: Matrix[{row},{col}]=?");
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    //Console.WriteLine($"Reading: Matrix[{row},{col}]=?");
+                    //Console.Write(matrix[row,col] + " ");
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+                sum = 0;
+            }
         }
     }
 }
