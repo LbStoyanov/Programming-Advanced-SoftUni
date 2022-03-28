e939ca624ffe3888b83cc55dc56249ac8229f3a7
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-28 10:01:12 +0300

    Problem solved.New problem added

--- Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
+++ Multidimensional Arrays - Lab/4. Symbol in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4._Symbol_in_Matrix
 {
@@ -6,7 +7,37 @@ namespace _4._Symbol_in_Matrix
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+                }
+            }
+
+
+            char symbol = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    var currentChar = matrix[row, col];
+                    if (currentChar == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix ");
         }
     }
 }
