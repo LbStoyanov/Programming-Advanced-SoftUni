ed96eb39f9bf6b12d043ce71e6ce18e28f2ae19b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-28 19:55:47 +0300

    Current problem solved.New problem added

--- Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
+++ Multidimensional Arrays - Exercise/5. Snake Moves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5._Snake_Moves
 {
@@ -6,7 +7,53 @@ namespace _5._Snake_Moves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] stairsSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = stairsSize[0];
+            int cols = stairsSize[1];
+
+            string snake = Console.ReadLine();
+
+            char[,] snakeCondition = new char[rows, cols];
+
+            int counter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (counter == snake.Length)
+                    {
+                        counter = 0;
+                    }
+                    char currentChar = snake[counter];
+                    snakeCondition[row, col] = currentChar;
+                    counter++;
+                }
+            }
+
+            for (int row = 0; row < snakeCondition.GetLength(0); row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < snakeCondition.GetLength(1); col++)
+                    {
+
+                        Console.Write(snakeCondition[row, col]);
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int col = snakeCondition.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        Console.Write(snakeCondition[row, col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
         }
     }
 }
