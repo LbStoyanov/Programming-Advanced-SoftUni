6983e065dbc8a379a5470168c7b7209d49cea538
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-28 12:15:53 +0300

    All problems solved.New project added.

--- Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
+++ Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -6,7 +6,25 @@ namespace _7._Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            
+            int rows = int.Parse(Console.ReadLine());
+            long[][] pascalTriangle = new long[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;  
+                pascalTriangle[row][^1] = 1; 
+
+                for (int col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[row]));
+            }
         }
     }
 }
