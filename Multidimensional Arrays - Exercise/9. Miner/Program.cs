5b9e1d0d21db6db6891793511fa36dd5c246523c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-03-29 16:50:03 +0300

    Half of the problem has resolved.

--- Multidimensional Arrays - Exercise/9. Miner/Program.cs
+++ Multidimensional Arrays - Exercise/9. Miner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _9._Miner
 {
@@ -6,7 +7,66 @@ namespace _9._Miner
     {
         static void Main(string[] args)
         {
-            
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            string[,] field = new string[fieldSize, fieldSize];
+
+            string[] moveDirections = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    field[row, col] = input[col];
+                }
+            }
+
+            int minerStartRowPosition = 0;
+            int minerStartColPosition = 0;
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    if (field[row,col] == "s")
+                    {
+                        minerStartRowPosition = row;
+                        minerStartColPosition = col;
+                    }
+                }
+            }
+
+          
+
+            for (int i = 0; i < moveDirections.Length; i++)
+            {
+                string currentDirection = moveDirections[i];
+
+                if (currentDirection == "up" )
+                {
+
+                }
+                if (currentDirection == "down")
+                {
+
+                }
+                if (currentDirection == "right")
+                {
+
+                }
+                if (currentDirection == "left")
+                {
+
+                }
+
+
+            }
+        }
+        private static bool isInRange(int[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
