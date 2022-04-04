92c9a2786861a80c1b9050915062b6e1a7ba7ab0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-04 16:44:08 +0300

    Problem solved

--- Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
+++ Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05._Cities_by_Continent_and_Country
 {
@@ -6,7 +7,44 @@ namespace _05._Cities_by_Continent_and_Country
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfCountries = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < numberOfCountries; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>> { { country, new List<string> { city } } });
+                }
+                else
+                {
+                    if (!continents[continent].ContainsKey(country))
+                    {
+                        continents[continent].Add(country, new List<string> { city });
+                    }
+                    else
+                    {
+                        continents[continent][country].Add(city);
+                    }
+                }
+            }
+
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continents[continent.Key])
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ",country.Value)}");
+                }
+            }
         }
     }
 }
