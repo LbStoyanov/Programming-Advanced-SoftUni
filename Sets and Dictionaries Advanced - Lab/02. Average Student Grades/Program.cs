763433f467b898ecf976d66b5da20406c247b79e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-04 16:17:07 +0300

    New problem added

--- Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -9,7 +9,7 @@ namespace _02._Average_Student_Grades
         static void Main(string[] args)
         {
             int numberOfStudents = int.Parse(Console.ReadLine());
-            var studentsGradesDict = new Dictionary<string, List<double>>();
+            var studentsGradesDict = new Dictionary<string, List<decimal>>();
 
             for (int i = 0; i < numberOfStudents; i++)
             {
@@ -17,25 +17,32 @@ namespace _02._Average_Student_Grades
 
                 string currentStudentName = currentStudentInformation[0];
 
-                double currentStudentGrade =  double.Parse(currentStudentInformation[1]);
+                decimal currentStudentGrade =  decimal.Parse(currentStudentInformation[1]);
 
                 if (!studentsGradesDict.ContainsKey(currentStudentName))
                 {
-                    studentsGradesDict.Add(currentStudentName, new List<double>());
+                    studentsGradesDict.Add(currentStudentName, new List<decimal>());
                 }
 
                 studentsGradesDict[currentStudentName].Add(currentStudentGrade);
             }
 
             foreach (var item in studentsGradesDict)
-            {
+             {
                 var name = item.Key;
 
-                var grades = string.Join(" ",item.Value);
+                var grades = item.Value;
 
                 var average = item.Value.Average();
 
-                Console.WriteLine($"{name} -> {grades:f2} (avg: {average:f2})");
+                Console.Write($"{name} -> ");
+
+                foreach (var grade in grades)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                Console.WriteLine($"(avg: {average:f2})");
             }
         }
     }
--- Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
+++ Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04._Product_Shop
 {
@@ -6,7 +7,38 @@ namespace _04._Product_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string command;
+            SortedDictionary<string,List<string>> shopDict = new SortedDictionary<string,List<string>>();
+
+            while ((command = Console.ReadLine()) != "Revision")
+            {
+                string[] splitedCommand = command.Split(", ",StringSplitOptions.RemoveEmptyEntries);
+
+                string shopName = splitedCommand[0];
+                string product = splitedCommand[1];
+                double price = double.Parse(splitedCommand[2]);
+
+                if (!shopDict.ContainsKey(shopName))
+                {
+                    shopDict.Add(shopName, new List<string>());
+                    shopDict[shopName].Add(product);
+                    shopDict[shopName].Add(price.ToString());
+                }
+                else
+                {
+                    if (shopDict[shopName][0] == product)
+                    {
+                        shopDict[shopName].Add(price.ToString());
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+
+
+            }
         }
     }
 }
