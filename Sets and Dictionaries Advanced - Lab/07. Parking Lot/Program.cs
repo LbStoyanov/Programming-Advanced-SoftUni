dc5916811e921ddf5693cf4568600ae7ea282bb3
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-04 20:27:05 +0300

    Problem solved

--- Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07._Parking_Lot
 {
@@ -6,7 +7,38 @@ namespace _07._Parking_Lot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> cars = new HashSet<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] splitted = command.Split(", ");
+
+                string direction = splitted[0];
+                string carNumber = splitted[1];
+
+
+
+                if (direction == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else
+                {
+                    cars.Remove(carNumber);
+                }
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                Environment.Exit(0);
+            }
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
