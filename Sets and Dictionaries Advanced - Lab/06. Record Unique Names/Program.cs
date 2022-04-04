28031ed03d216828e404486d38014ad59e08322e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-04 16:48:00 +0300

    Problem solved.

--- Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
+++ Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06._Record_Unique_Names
 {
@@ -6,7 +7,17 @@ namespace _06._Record_Unique_Names
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string currentName = Console.ReadLine();
+                uniqueNames.Add(currentName);
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join("\n",uniqueNames));
         }
     }
 }
