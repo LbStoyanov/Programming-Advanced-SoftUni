5402bcf0208396096f7ba890f3222b6d66692d83
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-04 21:23:21 +0300

    Problem solved.Project finised.New project added

--- Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08._SoftUni_Party
 {
@@ -6,7 +7,68 @@ namespace _08._SoftUni_Party
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string command;
+           
+
+            while ((command = Console.ReadLine()) != "PARTY")
+            {
+                //isValid
+                if (command.Length != 8)
+                {
+                    continue;
+                }
+                else
+                {
+                    //isVIP
+                    if (IsReservationVipType(command))
+                    {
+                        vipGuests.Add(command);
+                    }
+                    else
+                    {
+                        regularGuests.Add(command);
+                    }
+                }
+            }
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (vipGuests.Contains(command))
+                {
+                    vipGuests.Remove(command);
+                }
+                else if (regularGuests.Contains(command))
+                {
+                    regularGuests.Remove(command);
+                }
+            }
+            var totalCount = vipGuests.Count + regularGuests.Count;
+            Console.WriteLine(totalCount);
+
+            foreach (var item in vipGuests)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in regularGuests)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static bool IsReservationVipType(string command)
+        {
+            if (char.IsDigit(command[0]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
+    
 }
