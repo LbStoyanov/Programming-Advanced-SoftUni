42aa007324594c685ae4f5c6335239a1d1a41181
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-05 09:45:13 +0300

    Problem solved.New problem added.

--- Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
+++ Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Sets_of_Elements
 {
@@ -6,7 +8,37 @@ namespace _02._Sets_of_Elements
     {
         static void Main(string[] args)
         {
-            
+            int[] numberOfElements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            int firstSetElements = numberOfElements[0];//3
+            int secondSetElements = numberOfElements[1];//4
+            int totalElements = firstSetElements + secondSetElements;
+
+            for (int i = 0; i < totalElements; i++)
+            {
+                int currentElement = int.Parse(Console.ReadLine());
+
+
+                if (i >= firstSetElements)
+                {
+                    secondSet.Add(currentElement);
+                }
+                else
+                {
+                    firstSet.Add(currentElement);
+                }
+            }
+
+            foreach (var item in firstSet)
+            {
+                if (secondSet.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
         }
     }
 }
