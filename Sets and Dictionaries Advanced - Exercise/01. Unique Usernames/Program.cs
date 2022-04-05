1266aa31093a7ab1ef72d4e774bbf9d1400b0d5f
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-05 09:14:30 +0300

    Problem solved.New problem added

--- Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
+++ Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Unique_Usernames
 {
@@ -6,7 +7,18 @@ namespace _01._Unique_Usernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join("\n",uniqueNames));
         }
     }
 }
+ 
