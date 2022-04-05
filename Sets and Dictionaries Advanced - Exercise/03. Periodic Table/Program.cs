e7eb334d469a613719836c5cd78ea4e3a0c377b4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-05 10:00:18 +0300

    Problem solved.New problem added

--- Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
+++ Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._Periodic_Table
 {
@@ -6,7 +8,24 @@ namespace _03._Periodic_Table
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> uniqueElements = new HashSet<string>();
+
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    uniqueElements.Add(input[j]);
+                }
+            }
+
+            foreach (var item in uniqueElements.OrderBy(x =>x))
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
