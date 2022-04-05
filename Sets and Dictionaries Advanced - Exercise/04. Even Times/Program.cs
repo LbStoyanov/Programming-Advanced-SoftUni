77d2eff8c9ebcb872aadaafc80a38fba7b554813
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-05 10:09:33 +0300

    Problem solved.New problem added

--- Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
+++ Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Even_Times
 {
@@ -6,7 +8,42 @@ namespace _04._Even_Times
     {
         static void Main(string[] args)
         {
-            
+
+            var numbersAndCount = new Dictionary<int, int>();
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                if (numbersAndCount.ContainsKey(number))
+                {
+                    numbersAndCount[number]++;
+                }
+                else
+                {
+                    numbersAndCount.Add(number, 1);
+                }
+            }
+
+            var evenTimesNumber = numbersAndCount.First(kvp => kvp.Value % 2 == 0).Key;
+
+            Console.WriteLine(evenTimesNumber);
+
+            //HashSet<int> uniqueElements = new HashSet<int>();
+
+            //int countOfIntegers = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < countOfIntegers; i++)
+            //{
+            //    int currentElement = int.Parse(Console.ReadLine());
+
+            //    if (uniqueElements.Contains(currentElement))
+            //    {
+            //        Console.WriteLine(currentElement);
+            //        return;
+            //    }
+
+            //    uniqueElements.Add(currentElement);
+            //}
         }
     }
 }
