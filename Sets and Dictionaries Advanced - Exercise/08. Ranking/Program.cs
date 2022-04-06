1f1da6816006ef8e5658871565a6154c16cc70eb
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-06 15:43:13 +0300

    Problem solved.New problem added.

--- Sets and Dictionaries Advanced - Exercise/08. Ranking/Program.cs
+++ Sets and Dictionaries Advanced - Exercise/08. Ranking/Program.cs
@@ -1,14 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _08._Ranking
+namespace SoftUni
 {
-    
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-                        
+            var command = Console.ReadLine();
+            var contestDictionary = new Dictionary<string, string>();
+
+            while (command != "end of contests")
+            {
+                AddContest(command, contestDictionary);
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            var userDictionary = new Dictionary<string, Dictionary<string, int>>();
+
+            while (command != "end of submissions")
+            {
+                AddStudentInfoForContest(command, contestDictionary, userDictionary);
+
+                command = Console.ReadLine();
+            }
+
+            PrintResult(userDictionary);
+        }
+
+        public static void AddContest(string command, Dictionary<string, string> contestDictionary)
+        {
+            var splittedComamndArrya = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
+            var contest = splittedComamndArrya[0];
+            var password = splittedComamndArrya[1];
+
+            if (!contestDictionary.ContainsKey(contest))
+            {
+                contestDictionary.Add(contest, password);
+            }
+            else
+            {
+                contestDictionary[contest] = password;
+            }
+        }
+
+        public static void AddStudentInfoForContest(string command, Dictionary<string, string> contestDictionary, Dictionary<string, Dictionary<string, int>> userDictionary)
+        {
+            var splittedComamndArrya = command.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+            var contest = splittedComamndArrya[0];
+            var password = splittedComamndArrya[1];
+            var username = splittedComamndArrya[2];
+            var points = int.Parse(splittedComamndArrya[3]);
+
+            if (contestDictionary.ContainsKey(contest) && contestDictionary[contest] == password)
+            {
+                if (!userDictionary.ContainsKey(username))
+                {
+                    var userContestDictionary = new Dictionary<string, int> { { contest, points } };
+
+                    userDictionary.Add(username, userContestDictionary);
+                }
+                else
+                {
+                    if (!userDictionary[username].ContainsKey(contest))
+                    {
+                        userDictionary[username].Add(contest, points);
+                    }
+                    else
+                    {
+                        var oldPointForContest = userDictionary[username][contest];
+
+                        if (points > oldPointForContest)
+                        {
+                            userDictionary[username][contest] = points;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void PrintResult(Dictionary<string, Dictionary<string, int>> userDictionary)
+        {
+            var best = userDictionary.OrderByDescending(x => x.Value.Sum(y => y.Value)).First();
+
+            System.Console.WriteLine($"Best candidate is {best.Key} with total {best.Value.Sum(y => y.Value)} points.");
+            System.Console.WriteLine("Ranking: ");
+
+            foreach (var user in userDictionary.OrderBy(x => x.Key))
+            {
+                System.Console.WriteLine(user.Key);
+                foreach (var userContest in user.Value.OrderByDescending(x => x.Value))
+                {
+                    System.Console.WriteLine($"#  {userContest.Key} -> {userContest.Value}");
+                }
+            }
         }
     }
 }
---
+++ Sets and Dictionaries Advanced - Exercise/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> participantsInTheContest = new Dictionary<string, int>();
+
+            Dictionary<string, int> languagesAndNumerOfSubmitions = new Dictionary<string, int>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "exam finished")
+            {
+                string[] input = command.Split('-');
+                string userName = input[0];
+
+                if (input[1] == "banned")
+                {
+                    participantsInTheContest.Remove(userName);
+                    continue;
+                }
+                
+                string language = input[1];
+                int points = int.Parse(input[2]);
+
+                if (!participantsInTheContest.ContainsKey(userName))
+                {
+                    participantsInTheContest.Add(userName, points);
+                }
+                else
+                {
+                    if (participantsInTheContest[userName] < points)
+                    {
+                        participantsInTheContest[userName] = points;
+                    }
+                }
+
+                if (!languagesAndNumerOfSubmitions.ContainsKey(language))
+                {
+                    languagesAndNumerOfSubmitions.Add(language, 0);
+                }
+
+                languagesAndNumerOfSubmitions[language]++;
+
+            }
+
+            var orderedUsersDict = participantsInTheContest.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            Console.WriteLine("Results:");
+
+            foreach (var participant in orderedUsersDict)
+            {
+                Console.WriteLine($"{participant.Key} | {participant.Value}");
+            }
+
+            var orderedLanguageDict = languagesAndNumerOfSubmitions.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var language in orderedLanguageDict)
+            {
+                Console.WriteLine($"{language.Key} - {language.Value}");
+            }
+        }
+    }
+}
