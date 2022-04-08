71fde3239617120dab8d9d009f065007e8393d70
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-08 16:15:24 +0300

    Project finished.

--- Functional Programming - Lab/04. Add VAT/Program.cs
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _04._Add_VAT
@@ -7,14 +8,22 @@ namespace _04._Add_VAT
     {
         static void Main(string[] args)
         {
-            double[] prices = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+            //decimal[] prices = Console.ReadLine().Split(", ").Select(decimal.Parse).ToArray();
 
-            prices = prices.Select(x => x + x * 0.20).ToArray();
+            List<decimal> prices = Console.ReadLine().Split(", ").Select(decimal.Parse).ToList();
 
-            foreach (var item in prices)
-            {
-                Console.WriteLine($"{item:f2}");
-            }
+            Func<decimal, decimal> vatAdder = x => x + x * 0.20m;
+
+            //prices = prices.Select(x => x + x * 0.20m).ToList();
+
+            prices = prices.Select(vatAdder).ToList();
+
+            prices.ForEach(x => Console.WriteLine($"{x:f2}"));
+
+            //foreach (var item in prices)
+            //{
+            //    Console.WriteLine($"{item:f2}");
+            //}
         }
     }
 }
--- Functional Programming - Lab/05. Filter By Age/Program.cs
+++ Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Filter_By_Age
 {
@@ -6,7 +8,113 @@ namespace _05._Filter_By_Age
     {
         static void Main(string[] args)
         {
-            
+            int lines = int.Parse(Console.ReadLine());
+
+            var persons = new Dictionary<string, int>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(", ");
+
+                string name = input[0];
+
+                int currentAge = int.Parse(input[1]);
+
+                if (!persons.ContainsKey(name))
+                {
+                    persons.Add(name, currentAge);
+                }
+     
+            }
+
+            string condition = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+
+            string[] format = Console.ReadLine().Split(" ").ToArray();
+
+            if (condition == "younger")
+            {
+                IsYounger(persons,format,age);
+            }
+            else
+            {
+                IsOlder(persons, format, age);
+            }
+        }
+
+        private static void IsOlder(Dictionary<string, int> persons, string[] format, int age)
+        {
+            if (format.Length == 1)
+            {
+                if (format[0] == "name")
+                {
+                    foreach (var person in persons)
+                    {
+                        if (person.Value > age)
+                        {
+                            Console.WriteLine(person.Key);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var person in persons)
+                    {
+                        if (person.Value >= age)
+                        {
+                            Console.WriteLine(person.Value);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var person in persons)
+                {
+                    if (person.Value >= age)
+                    {
+                        Console.WriteLine($"{person.Key} - {person.Value}");
+                    }
+                }
+            }
+        }
+
+        private static void IsYounger(Dictionary<string, int> persons, string[] format,int age)
+        {
+            if (format.Length == 1)
+            {
+                if (format[0] == "name")
+                {
+                    foreach (var person in persons)
+                    {
+                        if (person.Value < age)
+                        {
+                            Console.WriteLine(person.Key);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var person in persons)
+                    {
+                        if (person.Value <= age)
+                        {
+                            Console.WriteLine(person.Value);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var person in persons)
+                {
+                    if (person.Value <= age)
+                    {
+                        Console.WriteLine($"{person.Key} - {person.Value}");
+                    }
+                }
+            }
         }
     }
 }
