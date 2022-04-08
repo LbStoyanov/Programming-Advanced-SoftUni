84647d04434445e818cbb71098b4cdd385e5ec72
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-08 19:37:38 +0300

    Problem solved.New problem added.

---
+++ Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._Car_Extension
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        public Car()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - (FuelConsumption * distance) > 0)
+            {
+                FuelQuantity -= fuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ Defining Classes - Lab/02. Car Extension/Program.cs
@@ -0,0 +1,20 @@
+﻿using _02._Car_Extension;
+using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "vw";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 2000;
+            car.FuelConsumption = 200;
+            car.Drive(2);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
