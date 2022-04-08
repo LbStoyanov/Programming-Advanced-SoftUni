3e2196e047194ab4e97195e242e7be099bea2f90
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-08 10:33:53 +0300

    Problem resolved.New problem added

--- Streams, Files and Directories - Exercise/Skeleton/CopyDirectory/CopyDirectory.cs
+++ Streams, Files and Directories - Exercise/Skeleton/CopyDirectory/CopyDirectory.cs
@@ -1,6 +1,8 @@
 ﻿namespace CopyDirectory
 {
     using System;
+    using System.IO;
+
     public class CopyDirectory
     {
         static void Main(string[] args)
@@ -13,7 +15,22 @@
 
         public static void CopyAllFiles(string inputPath, string outputPath)
         {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath);
+            }
+            else
+            {
+                Directory.CreateDirectory(outputPath);
+            }
 
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (var file in files)
+            {
+                string fileName = outputPath + "\\" + Path.GetFileName(file);
+                File.Copy(file, fileName);
+            }
         }
     }
 }
