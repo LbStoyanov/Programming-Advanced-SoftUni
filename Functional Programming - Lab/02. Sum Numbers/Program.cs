8724eafd89989e3f85693119023ea9faa0570c3c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-08 13:14:31 +0300

    Problem solved.New problem added.

---
+++ Functional Programming - Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,int> parser = x => int.Parse(x);
+
+            int[] numbers = Console.ReadLine().Split(", ").Select(parser).ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+            
+        }
+    }
+}
