83b0e08d66e3b73b09b9974102a782f3dc8eb32c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-12 08:06:54 +0300

    Problem resolved

--- Defining Classes - Lab/03. Car Constructors/Car.cs
+++ Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -30,18 +30,24 @@ namespace CarManufacturer
             FuelConsumption = fuelConsumption;
             FuelQuantity = fuelQuantity;
         }
-        
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption,Engine engine,Tire[] tires) : this(make, model, year,fuelQuantity,fuelConsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
         public string Make { get { return this.make; } set { this.make = value; } }
         public string Model { get { return this.model; } set { this.model = value; } }
         public int Year { get { return this.year; } set { this.year = value; } }
         public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
         public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
-
+        public Engine Engine  { get; set; }
+        public Tire[] Tires { get; set; }
         public void Drive(double distance)
         {
-            if (FuelQuantity - (FuelConsumption * distance) > 0)
+            if (fuelQuantity - distance * fuelConsumption > 0)
             {
-                FuelQuantity -= fuelConsumption * distance;
+                fuelQuantity -= distance * fuelConsumption;
             }
             else
             {
--- Defining Classes - Lab/03. Car Constructors/Program.cs
+++ Defining Classes - Lab/03. Car Constructors/Program.cs
@@ -7,15 +7,7 @@ namespace CarManufacturer
         static void Main(string[] args)
         {
           
-            string make = Console.ReadLine();
-            string model = Console.ReadLine();
-            int year = int.Parse(Console.ReadLine());
-            double fuelQuantity = double.Parse(Console.ReadLine());
-            double fuelConsuption = double.Parse(Console.ReadLine());
-
-            Car firstCar = new Car();
-            Car secondCar = new Car(make,model,year);
-            Car thirdCar = new Car(make,model,year,fuelQuantity,fuelConsuption);
+           
         }
     }
 }
