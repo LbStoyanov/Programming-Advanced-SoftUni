41e261fe0a02efefed8df65a5898bcac2c654d89
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-12 13:55:30 +0300

    Problem solved.New problem added.

--- DefiningClasses/01. Define a Class Person/StartUp.cs
+++ DefiningClasses/01. Define a Class Person/StartUp.cs
@@ -7,7 +7,24 @@ namespace DefiningClasses
         static void Main(string[] args)
         {
             
+            int n = int.Parse(Console.ReadLine());
 
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] intput = Console.ReadLine().Split();
+
+                string name = intput[0];
+                int age = int.Parse(intput[1]);
+                Person person = new Person(name,age);
+
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
         }
     }
 }
