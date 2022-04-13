ba3b5e9d2105e99f9a44f21640ad181c011dfa76
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-13 14:09:31 +0300

    Code improved.

--- DefiningClasses/CarSalesman/StartUp.cs
+++ DefiningClasses/CarSalesman/StartUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DefiningClasses
 {
@@ -6,7 +7,31 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            int linesOfEngines = int.Parse(Console.ReadLine());
+
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < linesOfEngines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string model = input[0];
+                int power = int.Parse(input[1]);
+
+            }
+
             
+
+            int linesOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < linesOfCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                
+            }
+
         }
     }
 }
