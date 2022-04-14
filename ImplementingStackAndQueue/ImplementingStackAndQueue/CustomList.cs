b2ca9fc16d92050b0a1da96d0ad9a3aa33c9d573
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-14 18:53:10 +0300

    CustomListImplementation

--- ImplementingStackAndQueue/ImplementingStackAndQueue/CustomList.cs
+++ ImplementingStackAndQueue/ImplementingStackAndQueue/CustomList.cs
@@ -1,4 +1,6 @@
-﻿namespace ImplementingStackAndQueue
+﻿using System;
+
+namespace ImplementingStackAndQueue
 {
     public class CustomList
     {
@@ -9,36 +11,71 @@
             elements = new int[2];
             counter = 0;
         }
+        public int Count 
+        {
+            get { return counter; }
+        }
+
+        public int this[int i] 
+        {
+            get
+            {
+                EnsureIsInRange(i);
+
+                return elements[i];
+
+            }
+
+            set 
+            {
+                EnsureIsInRange(i);
+
+                elements[i] = value; 
+            }
+        }
+
+       
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            int firstElement = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = firstElement;
+        }
+
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                if (elements[i] == element)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
         public int RemoveAt(int index)
         {
+            EnsureIsInRange(index);
+
             int number = elements[index];
+            counter--;
 
-            elements[index] = 0;
-
-            for (int i = 0; i < counter; i++)
+            for (int i = index; i < counter; i++)
             {
                 elements[i] = elements[i + 1];
             }
 
             return number;
         }
-
         public void Add(int element)
         {
-            if (elements.Length == counter)
-            {
-                int[] copyArray = new int[elements.Length * 2];
-
-                for (int i = 0; i < elements.Length; i++)
-                {
-                    copyArray[i] = elements[i];
-                }
-
-                elements = copyArray; 
-            }
+            Resize();
             elements[counter] = element;
             counter++;
         }
+
         public void Shrink()
         {
             int[] newArray = new int[counter];
@@ -49,6 +86,27 @@
             }
 
             elements = newArray;
+        }
+        private void Resize()
+        {
+            if (elements.Length == counter)
+            {
+                int[] copyArray = new int[elements.Length * 2];
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    copyArray[i] = elements[i];
+                }
+
+                elements = copyArray;
+            }
+        }
+        private void EnsureIsInRange(int i)
+        {
+            if (i < 0 && i >= counter)
+            {
+                throw new IndexOutOfRangeException();
+            }
         }
     }
 }
--- ImplementingStackAndQueue/ImplementingStackAndQueue/Program.cs
+++ ImplementingStackAndQueue/ImplementingStackAndQueue/Program.cs
@@ -7,9 +7,23 @@ namespace ImplementingStackAndQueue
         static void Main(string[] args)
         {
             CustomList customList = new CustomList();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
             customList.Add(5);
-            customList.Add(10);
+            customList.Swap(1,2);
 
+            customList.Swap(-2,3);
+
+
+            Console.WriteLine(customList[0]);
+
+            for (int i = 0; i < customList.Count; i++)
+            {
+                Console.Write(customList[i] + ", ");
+            }
+            //Console.WriteLine(String.Join(", ",customList));
             //customList.RemoveAt(1);
         }
     }
