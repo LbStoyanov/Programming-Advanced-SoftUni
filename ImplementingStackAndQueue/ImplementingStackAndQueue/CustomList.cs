1aab3b4a778e6c0035ff33b710aecceb80bee0ab
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-14 16:57:15 +0300

    CustomListImplementation

---
+++ ImplementingStackAndQueue/ImplementingStackAndQueue/CustomList.cs
@@ -0,0 +1,54 @@
+﻿namespace ImplementingStackAndQueue
+{
+    public class CustomList
+    {
+        private int[] elements;
+        private int counter;
+        public CustomList()
+        {
+            elements = new int[2];
+            counter = 0;
+        }
+        public int RemoveAt(int index)
+        {
+            int number = elements[index];
+
+            elements[index] = 0;
+
+            for (int i = 0; i < counter; i++)
+            {
+                elements[i] = elements[i + 1];
+            }
+
+            return number;
+        }
+
+        public void Add(int element)
+        {
+            if (elements.Length == counter)
+            {
+                int[] copyArray = new int[elements.Length * 2];
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    copyArray[i] = elements[i];
+                }
+
+                elements = copyArray; 
+            }
+            elements[counter] = element;
+            counter++;
+        }
+        public void Shrink()
+        {
+            int[] newArray = new int[counter];
+
+            for (int i = 0; i < counter; i++)
+            {
+                newArray[i] = elements[i];
+            }
+
+            elements = newArray;
+        }
+    }
+}
---
+++ ImplementingStackAndQueue/ImplementingStackAndQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ImplementingStackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList customList = new CustomList();
+            customList.Add(5);
+            customList.Add(10);
+
+            //customList.RemoveAt(1);
+        }
+    }
+}
