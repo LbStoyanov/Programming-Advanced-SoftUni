5575dde8642bac4f5ea6f2885ceb46ef5ba76512
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-17 20:22:38 +0300

    Project finished.New project added.

---
+++ Generics - Lab/03. Generic Scale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(1,1);
+            
+
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}
