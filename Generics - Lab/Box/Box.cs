ce675dfb00cd62208293937491ca8dadaa7e4b1b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-17 19:37:54 +0300

    Problem resolved.New problem added.

---
+++ Generics - Lab/Box/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> internalList = new List<T>();
+        
+        public void  Add(T element)
+        {
+           this.internalList.Add(element);
+        }
+        public T Remove()
+        {
+            T elementForRemove = this.internalList.Last();
+
+            this.internalList.Remove(elementForRemove);
+
+            return elementForRemove;
+        }
+        public int Count()
+        {
+            return internalList.Count;
+        }
+    }
+}
---
+++ Generics - Lab/Box/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            
+            box.Add("1");
+            box.Add("2");
+            box.Add("3");
+
+            
+
+            box.Remove();
+
+            box.Print();
+            
+            Console.WriteLine("************************************************");
+            box.Remove();
+            box.Print();
+
+        }
+    }
+}
