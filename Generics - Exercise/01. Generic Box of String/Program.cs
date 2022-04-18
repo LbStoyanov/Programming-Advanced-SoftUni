bba6b759847a2d946191cb44353f2fe9b5c6690b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-18 11:21:27 +0300

    Problem solved.New problem added.

---
+++ Generics - Exercise/01. Generic Box of String/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+
+            int integerValue = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < integerValue; i++)
+            {
+                string stringValue = Console.ReadLine();
+
+                box.Items.Add(stringValue);
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}
