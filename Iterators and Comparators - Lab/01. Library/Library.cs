fe023c3667d98a1409d86ca26331c0b98c19e3c2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-20 12:57:35 +0300

    Problem solved.New problem added.

---
+++ Iterators and Comparators - Lab/01. Library/Library.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library()
+        {
+            this.books = new List<Book>();
+        }
+
+        public void Add(Book book)
+        {
+           this.books.Add(book);
+        }
+
+        public void Remove(Book book)
+        {
+            this.books.Remove(book);
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+           return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterators and Comparators - Lab/01. Library/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("The call",1999,"J.K. Bako");
+            
+            
+
+            Library library = new Library();
+            library.Add(book);
+            library.Add(book);
+            library.Add(book);
+            library.Add(book);
+            library.Add(book);
+
+            foreach (var item in library)
+            {
+                Console.WriteLine(String.Join(", ",item.Authors));
+            }
+
+        }
+    }
+}
