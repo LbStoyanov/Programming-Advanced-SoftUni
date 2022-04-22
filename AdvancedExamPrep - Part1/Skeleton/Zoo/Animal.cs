bdb581eae9a30e9e05801ec67b1e2aeb81971462
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-22 16:34:20 +0300

    Problem solved at 20%

---
+++ AdvancedExamPrep - Part1/Skeleton/Zoo/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        private string species;
+        private string diet;
+        private double weight;
+        private double lenght;
+
+        public Animal(string species,string diet,double weight,double lenght)
+        {
+            this.species = species;
+            this.diet = diet;
+            this.weight = weight;
+            this.lenght = lenght;
+        }
+
+        public override string ToString()
+        {
+            return $"The {this.species} is a {this.diet} and weighs {this.weight} kg.";
+        }
+    }
+}
---
+++ AdvancedExamPrep - Part1/Skeleton/Zoo/Zoo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Zoo
+{
+    public class Zoo 
+    {
+        public Zoo(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
+        public List<Animal> AnimalS { get; private set; }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
