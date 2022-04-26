11bc1a6357d470cafe3b73f8165f5e7075450ff9
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-26 15:04:18 +0300

    Problem solved at 35%

--- AdvancedExamPrep - Part1/Skeleton/Zoo/Zoo.cs
+++ AdvancedExamPrep - Part1/Skeleton/Zoo/Zoo.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Zoo
 {
-    public class Zoo 
+    public class Zoo
     {
         public Zoo(string name, int capacity)
         {
@@ -13,8 +14,38 @@ namespace Zoo
 
         public string Name { get; set; }
         public int Capacity { get; set; }
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            var animalsByDiet = new List<Animal>();
+
+            foreach (var animal in Animals.Where(x => x.Diet == diet))
+            {
+                animalsByDiet.Add(animal);
+            }
+            
+            return animalsByDiet;
+        }
+        public int RemoveAnimals(string species)
+        {
+            int removedAnimalsCount = 0;
+
+            foreach (Animal animal in Animals.Where(x => x.Species == species))
+            {
+                Animals.Remove(animal);
+                removedAnimalsCount++;
+            }
+
+            return removedAnimalsCount;
+        }
         public string AddAnimal(Animal animal)
         {
+            if (Capacity == 0)
+            {
+                return "The zoo is full.";
+            }
+
+            Capacity--;
+
             if (animal.Species == null || animal.Species == " ")
             {
                 return "Invalid animal species.";
@@ -23,11 +54,8 @@ namespace Zoo
             {
                 return "Invalid animal diet.";
             }
-            if (true)
-            {
 
-            }
-
+            Animals.Add(animal);
             return $"Successfully added {animal.Species} to the zoo.";
         }
     }
