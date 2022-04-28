8f9acba3fbfb9aa6c8bfb950eb27ea19230c3153
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-28 12:52:04 +0300

    Problem solved at 60%

---
+++ FishingNet/Fish.cs
@@ -0,0 +1,25 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+        private string fishType;
+        private double length;
+        private double weight;
+
+        public Fish(string fishType,double lenght,double weight)
+        {
+            this.FishType = fishType;
+            this.Lenght = lenght;
+            this.Weight = weight;
+        }
+        public string FishType { get; set; }
+        public double Lenght { get; set; }
+
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"There is a {this.fishType}, {this.length} cm. long, and {this.weight} gr. in weight.";
+        }
+    }
+}
---
+++ FishingNet/Net.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net 
+    {
+        public Net(string material, int capacity)
+        {
+            Material = material;
+            Capacity = capacity;
+            
+        }
+        public List<Fish> Fish { get; set; } = new List<Fish>();
+        
+
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+
+        public string AddFish(Fish fish)
+        {
+            if (Capacity == 0)
+            {
+                return "Fishing net is full.";
+            }
+
+            Capacity--;
+
+            if ((fish.FishType == null || fish.FishType == " ") && fish.Lenght == 0 || fish.Weight == 0)
+            {
+                return "Invalid fish.";
+            }
+            
+
+            Fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+        public bool ReleaseFish(double weight)
+        {
+            if (Fish.Any(x => x.Weight == weight))
+            {
+                Fish.RemoveAll(x => x.Weight == weight);
+                return true;
+            }
+
+            return false;
+        }
+        public Fish GetFish(string fishType)
+        {
+            return Fish.FirstOrDefault(x => x.FishType == fishType);
+        }
+
+        public Fish GetBiggestFish()
+        {
+            double maxLenght = 0;
+
+            foreach (var item in Fish)
+            {
+                if (item.Weight > maxLenght)
+                {
+                    maxLenght = item.Weight;
+                }
+            }
+
+            return Fish.Find(x => x.Weight == maxLenght);
+        }
+        public void Report()
+        {
+
+        }
+    }
+}
