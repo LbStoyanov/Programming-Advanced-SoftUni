05f52bea628d73fd263fc037c7394e3765f9ecf8
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-28 20:03:23 +0300

    Problem solved at 30%

--- AdvancedExamPrep - Part3/02. Armory/Program.cs
+++ AdvancedExamPrep - Part3/02. Armory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02._Armory
 {
@@ -6,7 +7,62 @@ namespace _02._Armory
     {
         static void Main(string[] args)
         {
-            
+            int armorySize = int.Parse(Console.ReadLine());
+
+            char[,] armory = new char[armorySize,armorySize];
+
+            int armyOfficerRowPosition = 0;
+            int armyOfficerColPosition = 0;
+
+            int[] officerCoordinates = new int[2];
+
+            for (int row = 0; row < armory.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().Replace(" ","").ToCharArray();
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    armory[row, col] = rowInput[col];
+
+                    if (armory[row, col] == 'A')
+                    {
+                        armyOfficerRowPosition = row;
+                        armyOfficerColPosition = col;
+
+                        officerCoordinates[0] = row;
+                        officerCoordinates[1] = col;
+                    }
+                }
+            }
+
+            string direction = Console.ReadLine();
+
+            while (true)
+            {
+                if (direction == "up")
+                {
+                    Move(armory,officerCoordinates);
+                }
+                if (direction == "down")
+                {
+                    
+                }
+                if (direction == "left")
+                {
+                    
+                }
+                if (direction == "right")
+                {
+                    
+                }
+
+                direction = Console.ReadLine();
+            }
+
+        }
+
+        public static  void Move(char[,] armory, int[] officerCoordinates)
+        {
+
         }
     }
 }
