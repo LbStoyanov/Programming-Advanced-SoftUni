cb8cfec61c620b3ad3e884d32fd9b5fa27248ff6
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-15 18:10:22 +0300

    Problem Food Finder solved at 20%.

---
+++ AdvancedExamPrep - Part4/01. Food Finder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wordsToBeCreated = new List<string>
+            {
+                "pear",
+                "flour",
+                "pork",
+                "olive",
+            };
+
+            Queue<char> vowels = new Queue<char>();
+            
+        }
+    }
+}
