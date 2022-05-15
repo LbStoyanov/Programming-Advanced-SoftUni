0355838da10912b30df81ba29394a4c4e0e16580
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-15 21:38:13 +0300

    Problem Food Finder solved at 60%.

--- AdvancedExamPrep - Part4/01. Food Finder/Program.cs
+++ AdvancedExamPrep - Part4/01. Food Finder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Food_Finder
 {
@@ -7,6 +8,8 @@ namespace _01._Food_Finder
     {
         static void Main(string[] args)
         {
+            List<string> finalListOfWords = new List<string>();
+
             List<string> wordsToBeCreated = new List<string>
             {
                 "pear",
@@ -15,8 +18,51 @@ namespace _01._Food_Finder
                 "olive",
             };
 
+            char[] volewsInput = Console.ReadLine().Replace(" ","").ToCharArray();
+            char[] consonantsInput = Console.ReadLine().Replace(" ","").ToCharArray();
+
+            Queue<char> vowels = AddVolewsInQueue(volewsInput);
+            Stack<char> consonants = AddConsonantInStack(consonantsInput);
+
+            while (consonants.Count != 0)
+            {
+                if (AddWordInFinalResult(vowels,consonants,wordsToBeCreated))
+                {
+
+                }
+            }
+
+
+        }
+
+        private static bool AddWordInFinalResult(Queue<char> vowels, Stack<char> consonants, List<string> wordsToBeCreated)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Stack<char> AddConsonantInStack(char[] consonantsInput)
+        {
+            Stack<char> consonants = new Stack<char>();
+
+            for (int i = consonantsInput.Length - 1; i >= 0; i--)
+            {
+                var currentChar = consonantsInput[i];
+                consonants.Push(currentChar);
+            }
+
+            return consonants;
+        }
+        public static Queue<char> AddVolewsInQueue(char[] volewsInput)
+        {
             Queue<char> vowels = new Queue<char>();
-            
+
+            for (int i = 0; i < volewsInput.Length; i++)
+            {
+                var currentChar = volewsInput[i];
+                vowels.Enqueue(currentChar);
+            }
+
+            return vowels;
         }
     }
 }
