2b1d07579ad52cab0f5d0d01ce4e0c7b34e1bb36
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-16 22:17:29 +0300

    Problem 02. Pawn Wars

---
+++ AdvancedExamPrep - Part4/02. Pawn Wars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _02._Pawn_Wars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] chessBoard = new char[8,8];
+
+            for (int row = 0; row < chessBoard.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    chessBoard[row, col] = rowInput[col];
+                }
+            }
+
+        }
+    }
+}
