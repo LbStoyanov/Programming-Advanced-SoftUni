1ea2cd2917dae4f42ff264f60864c3b94a756fca
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-16 23:49:37 +0300

    Problem solved at 30%

--- AdvancedExamPrep - Part4/02. Pawn Wars/Program.cs
+++ AdvancedExamPrep - Part4/02. Pawn Wars/Program.cs
@@ -7,7 +7,7 @@ namespace _02._Pawn_Wars
 {
     public class Pawn
     {
-        public Pawn(int row,int col)
+        public Pawn(int row, int col)
         {
             this.RowPosition = row;
             this.ColPosition = col;
@@ -22,7 +22,7 @@ namespace _02._Pawn_Wars
             Pawn whitePawn = new Pawn(0, 0);
             Pawn blackPawn = new Pawn(0, 0);
 
-            char[,] chessBoard = new char[8,8];
+            char[,] chessBoard = new char[8, 8];
 
             for (int row = 0; row < chessBoard.GetLength(0); row++)
             {
@@ -64,7 +64,7 @@ namespace _02._Pawn_Wars
             }
 
 
-            Console.WriteLine(new String('*',30));
+            Console.WriteLine(new String('*', 30));
             PrintResult(chessBoard);
 
         }
@@ -79,26 +79,34 @@ namespace _02._Pawn_Wars
             int row = whitePawn.RowPosition;
             int col = whitePawn.ColPosition;
 
-            if (isInTheChessBoard(chessBoard,row - 1,col))
+            if (isInTheChessBoard(chessBoard, row - 1, col - 1))
             {
                 if (chessBoard[row - 1, col - 1] == 'b')
                 {
                     chessBoard[row - 1, col - 1] = 'w';
+                    chessBoard[row, col] = '-';
                     return;
                 }
+            }
 
+            if (isInTheChessBoard(chessBoard, row - 1, col + 1))
+            {
                 if (chessBoard[row - 1, col + 1] == 'b')
                 {
                     chessBoard[row - 1, col + 1] = 'w';
+                    chessBoard[row, col] = '-';
                     return;
                 }
+            }
 
+            if (isInTheChessBoard(chessBoard, row - 1, col))
+            {
                 if (chessBoard[row - 1, col] != 'b')
                 {
                     chessBoard[row - 1, col] = 'w';
                     chessBoard[row, col] = '-';
+                    return;
                 }
-
             }
 
         }
@@ -109,15 +117,15 @@ namespace _02._Pawn_Wars
             {
                 for (int col = 0; col < chessBoard.GetLength(1); col++)
                 {
-                    Console.Write(chessBoard[row,col]);
+                    Console.Write(chessBoard[row, col]);
                 }
                 Console.WriteLine();
             }
 
         }
-        private static bool IsThereAnotherPawn(char[,] chessBoard,int row,int col)
+        private static bool IsThereAnotherPawn(char[,] chessBoard, int row, int col)
         {
-            return chessBoard[row,col] != '-';
+            return chessBoard[row, col] != '-';
         }
 
         private static bool isInTheChessBoard(char[,] chessBoard, int row, int col)
