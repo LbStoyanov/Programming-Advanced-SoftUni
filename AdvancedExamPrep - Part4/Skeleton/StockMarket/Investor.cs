cfffdef9e8dd0b90fb201778971e6b8b9565a5bd
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-17 13:57:52 +0300

    Problem 03. StockMarket is 30% solved.

---
+++ AdvancedExamPrep - Part4/Skeleton/StockMarket/Investor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string  BrokerName { get; set; }
+        public HashSet<Stock> Portfolio { get; set; }
+    }
+}
---
+++ AdvancedExamPrep - Part4/Skeleton/StockMarket/Stock.cs
@@ -0,0 +1,25 @@
+﻿namespace StockMarket
+{
+    public class Stock
+    {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+        }
+
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal  PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization 
+            => this.PricePerShare * this.TotalNumberOfShares;
+
+        public override string ToString()
+        {
+            return $"Company: {this.CompanyName}\nDirector: {this.Director}\nPrice per share: ${this.PricePerShare}\nMarket capitalization: ${this.MarketCapitalization}";
+        }
+    }
+}
