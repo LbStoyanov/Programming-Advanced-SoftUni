122755324bcb2ec145cc8e7ace6d3f1fc4211ac2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-18 12:56:52 +0300

    Working on solving the problem.20% of the solution proccessed.

--- AdvancedExamPrep - Part5/02. The Battle of The Five Armies/Program.cs
+++ AdvancedExamPrep - Part5/02. The Battle of The Five Armies/Program.cs
@@ -32,8 +32,46 @@
                 }
             }
 
-            Console.WriteLine();
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split();
+                string direction = commands[0];
+
+                int currentRow = int.Parse(commands[1]);
+                int currentCol = int.Parse(commands[2]);
+
+                middleWorldMap[currentRow, currentCol] = 'O';
+
+                if (direction == "up")
+                {
+
+                }
+                if (direction == "down")
+                {
+
+                }
+                if (direction == "left")
+                {
+
+                }
+                if (direction == "right")
+                {
+
+                }
+
+            }
+            
         }
 
+        public void MoveArmy(char[,] middleWorldMap,int row,int col)
+        {
+            
+        }
+
+        private static bool isInRange(char[,] middleWorldMap, int row, int col)
+        {
+            return row >= 0 && row < middleWorldMap.GetLength(0) && col >= 0 && col < middleWorldMap.GetLength(1);
+        }
     }
 }
