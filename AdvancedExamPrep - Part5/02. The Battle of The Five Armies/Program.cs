954f687d2ddb4d1b70a9e189d4d33da19ab36836
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-18 12:10:43 +0300

    Working on solving the problem.

--- AdvancedExamPrep - Part5/02. The Battle of The Five Armies/Program.cs
+++ AdvancedExamPrep - Part5/02. The Battle of The Five Armies/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace zadacha
+﻿namespace TheBattleOfFiveArmies
 {
     using System;
     using System.Linq;
@@ -8,7 +8,32 @@
     {
         static void Main(string[] args)
         {
+            int armyArmorValue = int.Parse(Console.ReadLine());
+            int numberOfRows = int.Parse(Console.ReadLine());
 
+            int armyRow = 0;
+            int armyCol = 0;
+
+            char[,] middleWorldMap = new char[numberOfRows,numberOfRows];
+
+            for (int row = 0; row < middleWorldMap.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    middleWorldMap[row, col] = rowInput[col];
+
+                    if (middleWorldMap[row, col] == 'A')
+                    {
+                        armyRow = row;
+                        armyCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine();
         }
+
     }
 }
