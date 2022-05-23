4b5a3ca527fe755ee99e2e53432710c3d189bafa
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-23 08:27:12 +0300

    CarRacing task

---
+++ AdvancedExamPrep - Part5/StreetRacing/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public Car(string make, string model, string licensePlate, int horesePower, double weight)
+        {
+            Make = make;
+            Model = model;
+            LicensePlate = licensePlate;
+            HoresePower = horesePower;
+            Weight = weight;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HoresePower { get; set; }
+        public double Weight { get; set; }
+
+
+    }
+}
