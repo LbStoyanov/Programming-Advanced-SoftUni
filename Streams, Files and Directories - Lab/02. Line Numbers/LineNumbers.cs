9307ca38c1e5e6d5f6f1cd7ae19738b551ff3b78
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-29 12:57:44 +0300

    Problem 02.Line Numbers Solved.

---
+++ Streams, Files and Directories - Lab/02. Line Numbers/LineNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+
+            StreamReader streamReader = new StreamReader(inputFilePath);
+
+            using (streamReader)
+            {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+
+                using (writer)
+                {
+                    int lineNum = 1;
+
+                    while (true)
+                    {
+                        string line = streamReader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        writer.WriteLine($"{lineNum}. {line}");
+                        lineNum++;
+                    }
+
+                }
+            }
+
+            
+
+        }
+    }
+
+}
