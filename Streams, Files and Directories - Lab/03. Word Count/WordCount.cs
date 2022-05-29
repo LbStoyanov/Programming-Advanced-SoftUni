35692d9fa6347d133436ca16284004922c1a7450
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-05-29 14:42:48 +0300

    Problem 03. Word Count Solved.

---
+++ Streams, Files and Directories - Lab/03. Word Count/WordCount.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string wordsFilePath = @"..\..\..\words.txt";
+            string textFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            CalculateWordCounts(wordsFilePath, textFilePath, outputFilePath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            StreamReader wordReader = new StreamReader(wordsFilePath);
+
+            using (wordReader)
+            {
+                string[] words = wordReader.ReadLine().Split();
+                
+                StringBuilder stringBuilder = new StringBuilder();
+
+                
+
+                StreamReader textReader = new StreamReader(textFilePath);
+
+                using (textReader)
+                {
+                    while (true)
+                    {
+                        string line = textReader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        stringBuilder.AppendLine(line);
+                        
+                    }
+                }
+
+                string[] wordsInText = stringBuilder.ToString()
+                    .Replace("!","")
+                    .Replace(".","")
+                    .Replace("...","")
+                    .Replace("-","")
+                    .Replace("?","")
+                    .Replace(",","")
+                    .Split();
+
+                StreamWriter streamWriter = new StreamWriter(outputFilePath);
+
+                var wordCountDict = new Dictionary<string, int>();
+
+                ExtractWordsCount(words, wordsInText, wordCountDict);
+
+                using (streamWriter)
+                {
+                    foreach (var item in wordCountDict.OrderByDescending(x => x.Value))
+                    {
+                        streamWriter.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                }
+
+            }
+        }
+
+        private static void ExtractWordsCount(string[] words, string[] wordsInText, Dictionary<string, int> wordCountDict)
+        {
+            foreach (var word in words)
+            {
+                for (int i = 0; i < wordsInText.Length; i++)
+                {
+                    string currentWord = wordsInText[i].ToLower();
+
+                    if (word == currentWord)
+                    {
+                        if (!wordCountDict.ContainsKey(word))
+                        {
+                            wordCountDict.Add(word, 1);
+                            continue;
+                        }
+
+                        wordCountDict[word]++;
+                    }
+                }
+            }
+        }
+    }
+}
