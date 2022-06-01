bda40cfe0ec1ea929106e9bfa7eda62094c7bcae
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-01 14:52:46 +0300

    Problem 05. Applied Arithmetics Solved.

--- FunctionalProgramming - Exercise/Functional Programming - Exercise/05. Applied Arithmetics/AppliedArithmetics.cs
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/05. Applied Arithmetics/AppliedArithmetics.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AppliedArithmetics
 {
@@ -6,7 +8,37 @@ namespace AppliedArithmetics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<List<int>,List<int>> func = list => list.Select(number => number += 1).ToList();
+
+            //Action<int> action = null;
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+
+                if (command == "add")
+                {
+                    func = list => list.Select(number => number += 1).ToList();
+                }
+                if (command == "multiply")
+                {
+                    func = list => list.Select(number => number *= 2).ToList();
+                }
+                if (command == "subtract")
+                {
+                    func = list => list.Select(number => number -= 1).ToList();
+                }
+                if (command == "print")
+                {
+                    Action<List<int>> action = num => Console.WriteLine(string.Join(" ", num));
+                    action(numbers);
+                    continue;
+                }
+
+                 numbers = func(numbers);
+            }
         }
     }
 }
