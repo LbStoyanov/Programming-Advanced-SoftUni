4631d580b3baa07c079d2b50522026d21b1c2795
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-01 17:42:37 +0300

    Problem 07. Predicate For Names Solved.

---
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/06. Reverse And Exclude/ReverseAndExclude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    internal class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            Action<List<int>> reverseAction = list => list.Reverse();
+
+            reverseAction(numbers);
+
+            Func<List<int>, List<int>> removeFunc = list => list.FindAll(x => x % divisibleNum != 0).ToList();
+
+            removeFunc(numbers).ForEach(x => Console.Write(x + " "));
+
+            //Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
---
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/07. Predicate For Names/PredicateForNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class PredicateForNames
+    {
+        static void Main(string[] args)
+        {
+            int wordLenght = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split().ToList();
+
+            Predicate<string> predicate = word => word.Length <= wordLenght;
+
+            Func<List<string>,List<string>> modifiedNames = list => list.FindAll(predicate);
+
+            modifiedNames(names).ForEach(x => Console.WriteLine(x));
+            
+
+            //Console.WriteLine(string.Join(Environment.NewLine,names));
+        }
+    }
+}
