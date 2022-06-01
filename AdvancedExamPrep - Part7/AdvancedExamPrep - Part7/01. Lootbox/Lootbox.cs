9225f60bd42e4d10be515a7723e8ee7990c0b2a4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-01 22:20:10 +0300

    Problem 01. Lootbox Solved.

---
+++ AdvancedExamPrep - Part7/AdvancedExamPrep - Part7/01. Lootbox/Lootbox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lootbox
+{
+    public class Lootbox
+    {
+        static void Main(string[] args)
+        {
+            int[] firstBoxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondBoxInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Predicate<int> predicate = num => num % 2 == 0;
+
+            Queue<int> firstBoxOfLoot = new Queue<int>(firstBoxInput);
+            Stack<int> secondBoxOfLoot = new Stack<int>(secondBoxInput);
+
+            int claimedItems = 0;
+
+            while (firstBoxOfLoot.Count != 0 && secondBoxOfLoot.Count != 0)
+            {
+                var firstLoot = firstBoxOfLoot.Peek();
+                var secondLoot = secondBoxOfLoot.Peek();
+
+                int obtainedValue = firstLoot + secondLoot;
+
+                bool isEven = predicate(obtainedValue);
+
+                if (isEven)
+                {
+                    claimedItems += obtainedValue;
+                    firstBoxOfLoot.Dequeue();
+                    secondBoxOfLoot.Pop();
+                }
+                else
+                {
+                    firstBoxOfLoot.Enqueue(secondBoxOfLoot.Pop());
+                }
+
+            }
+
+            if (firstBoxOfLoot.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (claimedItems >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {claimedItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {claimedItems}");
+            }
+
+
+        }
+    }
+}
---
+++ AdvancedExamPrep - Part7/AdvancedExamPrep - Part7/02. Re-Volt/ReVolt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReVolt
+{
+    public class ReVolt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
