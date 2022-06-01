193b38d93125414bdb3e436e0340ce9487be06a4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-01 13:07:28 +0300

    Problem 03. Custom Min Function Solved.

---
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/03. Custom Min Function/CustomMinFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class CustomMinFunction
+    {
+        static void Main(string[] args)
+        {
+            Func<HashSet<int>, int> returnSmallestNum = set => set.Min();
+
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+
+            Console.WriteLine(returnSmallestNum(numbers));
+        }
+    }
+}
