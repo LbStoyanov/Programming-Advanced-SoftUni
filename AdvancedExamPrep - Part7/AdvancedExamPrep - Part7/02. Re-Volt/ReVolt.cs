a530188269e6d3d056e00a834d9f52059b856818
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-01 22:48:46 +0300

    Problem 02. Re-Volt Solved at 50%.

--- AdvancedExamPrep - Part7/AdvancedExamPrep - Part7/02. Re-Volt/ReVolt.cs
+++ AdvancedExamPrep - Part7/AdvancedExamPrep - Part7/02. Re-Volt/ReVolt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ReVolt
 {
@@ -6,7 +7,73 @@ namespace ReVolt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixSize = int.Parse(Console.ReadLine());
+            int commandCount = int.Parse(Console.ReadLine());
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] rowInput = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < rowInput.Length; col++)
+                {
+                    matrix[row, col] = rowInput[col];
+
+                    if (matrix[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            for (int i = 0; i < commandCount; i++)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "up")
+                {
+
+                }
+                if (command == "down")
+                {
+
+                }
+                if (command == "left")
+                {
+
+                }
+                if (command == "right")
+                {
+
+                }
+            }
+
+
+            PrintResult(matrix);
+
+        }
+        private static bool isInRange(char[,] board, int row, int col)
+        {
+            return row >= 0 && row < board.GetLength(0) && col >= 0 && col < board.GetLength(1);
+        }
+
+
+        public static void PrintResult(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
