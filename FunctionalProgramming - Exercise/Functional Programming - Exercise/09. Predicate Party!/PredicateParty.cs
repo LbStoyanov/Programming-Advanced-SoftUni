28f83dd765a104da7e4d9b978409cb0a7f2f4bd2
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-02 18:02:08 +0300

    Problem 09. Predicate Party solved at 60%.

--- FunctionalProgramming - Exercise/Functional Programming - Exercise/09. Predicate Party!/PredicateParty.cs
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/09. Predicate Party!/PredicateParty.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PredicateParty
 {
@@ -6,7 +8,73 @@ namespace PredicateParty
     {
         static void Main(string[] args)
         {
-            
+            var personsList = Console.ReadLine().Split().ToList();
+            var personsGoingToTheParty = new List<string>();
+
+            Func<string, string> doubleThePerson = name => name + ", " + name;
+
+            string commands;
+
+            while ((commands = Console.ReadLine()) != "Party!")
+            {
+                string[] splittedCommand = commands.Split();
+                string mainCommand = splittedCommand[0];
+                string lettersForSearch = splittedCommand[2];
+
+                if (mainCommand == "Remove")
+                {
+                    if (splittedCommand[1] == "StartsWith")
+                    {
+                        personsList.RemoveAll(x => x.StartsWith(lettersForSearch));
+                    }
+                    else if (splittedCommand[1] == "EndsWith")
+                    {
+                        personsList.RemoveAll(x => x.EndsWith(lettersForSearch));
+                    }
+                }
+                if (mainCommand == "Double")
+                {
+                    if (splittedCommand[1] == "StartsWith")
+                    {
+                        string[] personsToBeDoubled = personsList.FindAll(x => x.StartsWith(lettersForSearch)).ToArray();
+
+                        for (int i = 0; i < personsToBeDoubled.Length; i++)
+                        {
+                            personsList.Add(doubleThePerson(personsToBeDoubled[i]));
+                        }
+                    }
+                    else if (splittedCommand[1] == "EndsWith")
+                    {
+                        string[] personsToBeDoubled = personsList.FindAll(x => x.EndsWith(lettersForSearch)).ToArray();
+
+                        for (int i = 0; i < personsToBeDoubled.Length; i++)
+                        {
+                            personsList.Add(doubleThePerson(personsToBeDoubled[i]));
+                        }
+                    }
+                    else
+                    {
+                        int lenghtToBeCompared = int.Parse(splittedCommand[2]);
+
+                        string[] personsToBeDoubled = personsList.FindAll(x => x.Length == lenghtToBeCompared).ToArray();
+
+                        for (int i = 0; i < personsToBeDoubled.Length; i++)
+                        {
+                            personsList.Add(doubleThePerson(personsToBeDoubled[i]));
+                        }
+                    }
+                }
+
+            }
+
+
+            if (personsList.Count == 0)
+            {
+                Console.WriteLine("Nobody is going to the party!");
+                return;
+            }
+            Console.Write(string.Join(", ",personsList));
+            Console.WriteLine(" are going to the party!");
         }
     }
 }
