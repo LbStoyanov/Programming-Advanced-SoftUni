12a708be3a4be4ab0b45309079a11753381c7aa9
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-03 10:44:50 +0300

    Problem 01.Warm Winter Solved.

---
+++ AdvancedExamPrep - Part8/AdvancedExamPrep - Part8/01.Warm Winter/WarmWinter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WarmWinter
+{
+    internal class WarmWinter
+    {
+        static void Main(string[] args)
+        {
+            List<int> hatsInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Stack<int> hats = new Stack<int>(hatsInput);
+
+
+            List<int> scarfsInput = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> scarfs = new Queue<int>(scarfsInput);
+
+            List<int> createdSets = new List<int>();
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                var currentHat = hats.Peek();
+                var currentScarf = scarfs.Peek();
+
+                if (currentHat > currentScarf)
+                {
+                    int setPrice = currentHat + currentScarf;
+                    createdSets.Add(setPrice);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+                else if(currentHat < currentScarf)
+                {
+                    hats.Pop();
+                }
+                else
+                {
+                    scarfs.Dequeue();
+
+                    int incriesedHatValue = hats.Pop() + 1;
+
+                    hats.Push(incriesedHatValue);
+
+                }
+            }
+            
+
+            int mostExpansiveSet = createdSets.OrderByDescending(x => x).FirstOrDefault();
+
+            Console.WriteLine($"The most expensive set is: {mostExpansiveSet}");
+
+            Console.WriteLine(String.Join(" ",createdSets));
+        }
+    }
+}
