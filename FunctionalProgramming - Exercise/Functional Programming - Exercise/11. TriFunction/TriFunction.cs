e6556fd6256ae338261680903472475241ef1376
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-03 09:47:51 +0300

    Problem 11.TriFunction Solved.

---
+++ FunctionalProgramming - Exercise/Functional Programming - Exercise/11. TriFunction/TriFunction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    internal class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int numberN = int.Parse(Console.ReadLine());
+
+            List<string> people = Console.ReadLine().Split().ToList();
+
+            foreach (var person in people)
+            {
+                int currentPersonValue = CalculateCharsSum(person);
+
+                if (currentPersonValue >= numberN)
+                {
+                    Console.WriteLine(person);
+                    break;
+                }
+            }
+
+        }
+
+        public static int CalculateCharsSum(string person)
+        {
+            int result = 0;
+
+            for (int j = 0; j < person.Length; j++)
+            {
+                var currentChar = person[j];
+
+                result += Convert.ToInt32(currentChar);
+            }
+
+            return result;
+        }
+    }
+}
