767703360d8365ebd9e823f08190d9dc2952c345
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-07 19:32:53 +0300

    Problem 02 Selling Solved.

--- AdvancedExamPrep - Part10/AdvancedExamPrep - Part10/02. Selling/Selling.cs
+++ AdvancedExamPrep - Part10/AdvancedExamPrep - Part10/02. Selling/Selling.cs
@@ -3,32 +3,133 @@ using System.Linq;
 
 namespace Selling
 {
+    class Player
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public int CollectedMoney { get; set; }
+    }
     internal class Selling
     {
         static void Main(string[] args)
         {
-            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Player player = new Player();
 
-            int rows = matrixSize[0];
-            int cols = matrixSize[1];
 
-            char[,] matrix = new char[rows, cols];
+            int matrixSize = int.Parse(Console.ReadLine());
+;
 
-            ReadingMatrix(matrix);
+            char[,] matrix = new char[matrixSize, matrixSize];
+
+            ReadingMatrix(matrix,player);
+
+            string direction = Console.ReadLine();
+
+            bool isOutOfTheBaker = false;
+
+            while (true)
+            {
+                matrix[player.Row, player.Col] = '-';
+
+                if (direction == "up")
+                {
+                    player.Row--;
+                }
+                if (direction == "down")
+                {
+                    player.Row++;
+                }
+                if (direction == "left")
+                {
+                    player.Col--;
+                }
+                if (direction == "right")
+                {
+                    player.Col++;
+                }
+
+                if (isInRange(matrix,player.Row,player.Col))
+                {
+                    MovePlayer(matrix, player);
+                }
+                else
+                {
+                    isOutOfTheBaker = true;
+                    break;
+                }
+
+                if (player.CollectedMoney >= 50)
+                {
+                    break;
+                }
+
+                direction = Console.ReadLine();
+            }
+
+            if (isOutOfTheBaker)
+            {
+                Console.WriteLine("Bad news, you are out of the bakery.");
+            }
+            else
+            {
+                Console.WriteLine("Good news! You succeeded in collecting enough money!");
+            }
+
+            Console.WriteLine($"Money: {player.CollectedMoney}");
+
+            PrintTheFinalStateOfTheMatrix(matrix);
         }
-        public void PrintTheFinalStateOfTheMatrix(char[,] matrix)
+
+        private static void MovePlayer(char[,] matrix, Player player)
+        {
+            if (matrix[player.Row,player.Col] == '-')
+            {
+                matrix[player.Row, player.Col] = 'S';
+            }
+            else if (matrix[player.Row, player.Col] == 'O')
+            {
+                matrix[player.Row, player.Col] = '-';
+                SearchingForSecondPillar(matrix,player);
+            }
+            else
+            {
+                int moneyForCollection = Convert.ToInt32(matrix[player.Row, player.Col] - 48);
+                player.CollectedMoney += moneyForCollection;
+                matrix[player.Row, player.Col] = 'S';
+            }
+            
+        }
+
+        private static void SearchingForSecondPillar(char[,] matrix,Player player)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    //Console.Write(matrix[row,col] + " ");
+                    if (matrix[row, col] == 'O')
+                    {
+                        matrix[row, col] = 'S';
+                        player.Row = row;
+                        player.Col = col;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public static void PrintTheFinalStateOfTheMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col]);
                 }
                 Console.WriteLine();
             }
 
         }
-        public static void ReadingMatrix(char[,] matrix)
+        public static void ReadingMatrix(char[,] matrix,Player player)
         {
             for (int row = 0; row < matrix.GetLength(0); row++)
             {
@@ -36,6 +137,11 @@ namespace Selling
                 for (int col = 0; col < rowInput.Length; col++)
                 {
                     matrix[row, col] = rowInput[col];
+                    if (matrix[row, col] == 'S')
+                    {
+                        player.Row = row;
+                        player.Col = col;
+                    }
                 }
             }
 
