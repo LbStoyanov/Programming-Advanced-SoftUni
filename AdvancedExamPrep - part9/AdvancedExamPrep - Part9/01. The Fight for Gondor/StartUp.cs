a1cc3939a61ea78f9886b4de2e2a2e57ee5dc7e9
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-07 10:07:39 +0300

    Problem 01.1. The Fight for Gondor Solved at 30%

---
+++ AdvancedExamPrep - part9/AdvancedExamPrep - Part9/01. The Fight for Gondor/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheFightforGondor
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int orcsWavesNumber = int.Parse(Console.ReadLine());
+
+            int[] platesInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> aragonsDefensePlates = new Queue<int>(platesInput);
+
+            int wavesCounter = 0;
+
+            for (int i = 0; i < orcsWavesNumber; i++)
+            {
+                if (wavesCounter == 3)
+                {
+                    int additionalPlate = int.Parse(Console.ReadLine());
+                    aragonsDefensePlates.Enqueue(additionalPlate);
+                    wavesCounter = 0;
+                }
+
+                int[] orcsInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                Stack<int> orcs = new Stack<int>(orcsInput);
+
+                while (true)
+                {
+                    var currentPlate = aragonsDefensePlates.Peek();
+                    var currentOrc = orcs.Peek();
+
+
+
+                }
+
+                
+                wavesCounter++;
+            }
+        }
+    }
+}
